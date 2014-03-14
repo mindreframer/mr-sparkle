d349f031f1cab91c8c2fa65b51392c95170ae2ae
Roman H <roman.heinrich@gmail.com>
2014-03-14 17:56:19 +0100

    also allow port configuration +
    more aggressive defaults(faster restarts)

--- lib/mr-sparkle/unicorn.conf.rb
+++ lib/mr-sparkle/unicorn.conf.rb
@@ -1,5 +1,6 @@
-worker_processes Integer(ENV["SPARKLE_WORKERS"] || 3)
-timeout Integer(ENV['SPARKLE_TIMEOUT'] || 60)
+worker_processes Integer(ENV["SPARKLE_WORKERS"] || 1)
+timeout          Integer(ENV['SPARKLE_TIMEOUT'] || 5)
+listen           Integer(ENV['SPARKLE_PORT']    || 3000)
 
 GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
 
